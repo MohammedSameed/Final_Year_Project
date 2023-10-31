034dd841e2dd9e3ec2bd34af5da720b83860ec14
MohammedSameed <mohammedsameed07@gmail.com>
2023-10-31 19:45:56 +0530

    Search bar and categories section added

--- chatbuddy/components/navbar.tsx
+++ chatbuddy/components/navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar =() => {
       </div>
       <div className="flex items-center gap-x-3">
         
-          <Button>
+          <Button variant = "premium" size = "sm">
             Upgrade
             <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
           </Button>
--- chatbuddy/components/sidebar.tsx
+++ chatbuddy/components/sidebar.tsx
@@ -6,6 +6,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 
+
 export const Sidebar = () => {
   
   const router = useRouter();
@@ -31,6 +32,10 @@ export const Sidebar = () => {
       pro: false,
     },
   ];
+   
+  const onNavigate = (url: string, pro:boolean) =>{
+    return router.push(url);
+  }
 
   return (
     <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
@@ -38,12 +43,15 @@ export const Sidebar = () => {
         <div className="space-y-2">
           {routes.map((route) => (
             <div
+            onClick={()=> onNavigate(route.href , route.pro)}
               key={route.href}
               className={cn(
                 "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
                 pathname === route.href && "bg-primary/10 text-primary",
               )}
+              
             >
+              
               <div className="flex flex-col gap-y-2 items-center flex-1">
                 <route.icon className="h-5 w-5" />
                 {route.label}
