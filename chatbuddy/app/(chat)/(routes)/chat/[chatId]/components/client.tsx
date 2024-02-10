4f7427af8637aebdd22fad5ebf6a047b6f948caa
MohammedSameed <mohammedsameed07@gmail.com>
2024-02-10 16:40:50 +0530

    create section and home section is completed (50%) of work done

---
+++ chatbuddy/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const ChatClient = () => {
+    return(
+        <div>
+            chat Id client
+        </div>
+    )
+}
---
+++ chatbuddy/app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -0,0 +1,13 @@
+interface ChatIdPageProps {
+    params: {
+      chatId: string;
+    }
+  }
+
+const ChatIdPage = ()=>{
+    return(
+
+        <div>hello chat id</div>
+    );
+}
+export default ChatIdPage;
